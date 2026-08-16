7dc75cbee02e821d2d93b16ebf17fb3dea1335e0
acabon29 <arthurcabon29@gmail.com>
2026-08-17 01:14:00 +0200

    correction

--- ex02/includes/MutantStack.hpp
+++ ex02/includes/MutantStack.hpp
@@ -9,27 +9,23 @@
 template <typename T>
 class MutantStack : public std::stack<T> {
 public:
+	// container_type est un typedef standard exposé publiquement par std::stack
+	// (il pointe vers le type du container sous-jacent, ici std::deque<T> par défaut).
 	typedef typename std::stack<T>::container_type::iterator iterator;
 	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
 	MutantStack();
 	MutantStack(const MutantStack& other);
 	MutantStack& operator=(const MutantStack& other);
 	~MutantStack();
 
+	// iterator : permet de lire et modifier l'élément pointé. *it = 42; fonctionne.
+	// const_iterator : permet seulement de lire l'élément pointé. *it = 42; ne compile pas — c'est un accès en lecture seule.
 	iterator begin();
 	iterator end();
 
 	const_iterator begin() const;
 	const_iterator end() const;
-
-	reverse_iterator rbegin();
-	reverse_iterator rend();
-
-	const_reverse_iterator rbegin() const;
-	const_reverse_iterator rend() const;
 };
 
 #include "MutantStack.tpp"
